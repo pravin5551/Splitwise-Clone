ec4ff1b6069e6e5096d2ac09bab1071910221e94
pravin5551 <pravinvpawar5551@gmail.com>
2021-03-09 12:41:18 +0530

    Splitwise first step

---
+++ app/src/main/java/com/exaple/splitwise_clone/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.exaple.splitwise_clone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
