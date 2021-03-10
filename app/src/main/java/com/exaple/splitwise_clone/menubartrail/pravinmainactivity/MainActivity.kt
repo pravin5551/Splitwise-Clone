ace0847e26481db01eeb1f7c06ab47157b3431a3
pravin5551 <pravinvpawar5551@gmail.com>
2021-03-10 17:59:40 +0530

    Pravin first comment

--- app/src/main/java/com/exaple/splitwise_clone/menubartrail/pravinmainactivity/MainActivity.kt
+++ app/src/main/java/com/exaple/splitwise_clone/menubartrail/pravinmainactivity/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.exaple.splitwise_clone
-
+package com.exaple.splitwise_clone.menubartrail.pravinmainactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.exaple.splitwise_clone.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/exaple/splitwise_clone/pravinTrialAndError/FragmentAdapter.kt
@@ -0,0 +1,33 @@
+package com.exaple.splitwise_clone.pravinTrialAndError
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+
+/**
+ * This class gives the fragments as the user swipes the screen horizontally
+ */
+class FragmentAdapter(fragmentActivity: FragmentActivity?) :
+    FragmentStateAdapter(fragmentActivity!!) {
+    /**
+     * This method is called when the user swipes the screen horizontally
+     *
+     * @param position position of the screen user wants to see
+     * @return returns a fragment based on the position
+     */
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            0 -> return FragmentA.newInstance("This is First Fragment, So Welcome to circle indicators. Hope you are doing Amazing")
+            1 -> return FragmentB.newInstance("This is second Fragment")
+            2 -> return FragmentC.newInstance("This is Third Fragment")
+            3 -> return FragmentD.newInstance("This is Third Fragment")
+
+        }
+        return FragmentA.newInstance("This is Default Fragment")
+    }
+
+    override fun getItemCount(): Int {
+        return 4
+    }
+}
