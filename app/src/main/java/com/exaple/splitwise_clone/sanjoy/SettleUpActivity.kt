59126e755561fe6766338b1749d07109598cecca
sanjoy <paulsanjoy9875@gmail.com>
2021-03-13 21:28:20 +0530

    last commit

--- app/src/main/java/com/exaple/splitwise_clone/sanjoy/SettleUpActivity.kt
+++ app/src/main/java/com/exaple/splitwise_clone/sanjoy/SettleUpActivity.kt
@@ -3,15 +3,10 @@ package com.exaple.splitwise_clone.sanjoy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.exaple.splitwise_clone.R
-import kotlinx.android.synthetic.main.activity_setting.*
 
-class SettingActivity : AppCompatActivity() {
+class SettleUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
-
-        cv5.setOnClickListener {
-
-        }
+        setContentView(R.layout.activity_settle_up)
     }
 }
